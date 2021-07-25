293d545e50aeb9fd7de46fca19976ac3ff0438f6
massivealice <70323782+massivealice@users.noreply.github.com>
2021-07-25 16:44:19 +0300

    Update String_concepts.py

--- Introduction/String_concepts.py
+++ Introduction/String_concepts.py
@@ -1,3 +1,3 @@
 first_name = input('What is your first name? ')
 last_name = input("What is your last name? ")
-print ('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
+print ('Hello, ' + first_name.capitalize() + ' ' + last_name.capitalize())
