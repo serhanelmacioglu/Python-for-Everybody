6e7878971c483be3ac04b53a8b0c9f5f47d34177
massivealice <70323782+massivealice@users.noreply.github.com>
2021-07-26 22:29:39 +0300

    Create Formatting_strings.py
    You can use one of the output strings above, bear in mind that I hereby prefer to use an output string that is executed based on Python 3.X merely.

---
+++ Introduction/Formatting_strings.py
@@ -0,0 +1,8 @@
+first_name = 'Alice'
+last_name = 'Liddell'
+
+# output = 'Hello, ' + first_name + ' ' + last_name
+# output = 'Hello, {} {}'.format(first_name, last_name)
+# output = 'Hello, {0} {1}'.format(first_name, last_name)
+output = f'Hello, {first_name} {last_name}'
+print (output)
