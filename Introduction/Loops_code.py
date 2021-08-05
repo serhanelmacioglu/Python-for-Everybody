d0c2cfd229060919e9f1e3c48240505c1c29fa24
Serhan Elmacıoğlu <70323782+serhanelmacioglu@users.noreply.github.com>
2021-08-05 16:14:59 +0300

    Create Loops_code.py

---
+++ Introduction/Loops_code.py
@@ -0,0 +1,17 @@
+# Loop through a collection
+
+for name in ['Christopher', 'Susan']:
+    print(name)
+
+# Looping a number of times
+
+for index in range(0,3):
+    print(index)
+
+# Looping with a condition
+names = ['Christopher', 'Susan']
+index = 0
+while index < len(names):
+    print(names[index])
+    # Change the condition!!
+    index = index + 1
