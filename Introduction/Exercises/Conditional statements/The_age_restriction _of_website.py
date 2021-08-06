5aa4145a009d70e78b91f9f211210389a574a56a
Serhan Elmacıoğlu <70323782+serhanelmacioglu@users.noreply.github.com>
2021-08-06 16:37:17 +0300

    Delete Introduction/Exercises/Conditional statements directory

--- Introduction/Exercises/Conditional statements/The_age_restriction _of_website.py
+++
@@ -1,8 +0,0 @@
-# The age restriction of website
-
-age = float(input('Enter your age: '))
-
-if age >= 18:
-    print('You can join us now, and enjoy it!')
-else:
-    print('We regrettably inform you that you will not be able to join our platform due to age restrictions.')
