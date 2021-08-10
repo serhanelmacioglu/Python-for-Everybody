84eca18ced2ca75191339a5cffcef422bf034d62
Serhan Elmacıoğlu <70323782+serhanelmacioglu@users.noreply.github.com>
2021-08-10 15:02:54 +0300

    Update helpers.py

--- Introduction/Virtual Environment Packages/helpers.py
+++ Introduction/Virtual Environment Packages/helpers.py
@@ -3,4 +3,5 @@ from pip._vendor.colorama import init, Fore
 def display(message, is_warning=False):
     if is_warning:
         print(Fore.RED + message)
-    print(Fore.BLUE + message)
+    else:
+        print(Fore.BLUE + message)
